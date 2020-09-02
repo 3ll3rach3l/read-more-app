7ad9bbe4c7e4e73d6bc8405481872599a8c89cab
Lauren <elle.rachel.writes@gmail.com>
2020-09-02 12:11:00 -1000

    sign up css is fixed. bug still an issue

--- client/src/pages/SignUpPage.js
+++ client/src/pages/SignUpPage.js
@@ -30,62 +30,53 @@ function SignUpPage() {
     dispatch(signup(name, email, password));
   };
 
-//   const handleSubmitDemo = (e) => {
-//     e.preventDefault();
-//     dispatch(login("demo@example.com", "password"));
-//   };
+
 
   if (isLoggedIn) return <Redirect to="/" />;
 
   return (
-    <Container fixed classes={{ root: classes.container }}>
-      <div className="main-container">
-        <div className="signup-form-container">
-          <h1>Sign up for readMore</h1>
-          <p className="subheading">
-            Sign up to start your reading challenge this year!
-          </p>
-          <div className="errors-container">
-            <ul className="errors" id="sign-up-errors"></ul>
-          </div>
-          
-            <form className="signup-form" onSubmit={handleSubmit}>
-              <label htmlFor="user_name">Name</label>
-              <AuthTextField
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                placeholder="Name"
-                variant="outlined"
-              />
-              <label htmlFor="user_email">Email address</label>
-              <AuthTextField
-                placeholder="you@yours.com"
-                variant="outlined"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <label htmlFor="user_password">Password</label>
-              <AuthTextField
-                type="password"
-                name="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <div className="signup-btn-container">
-                <AuthSubmitButton>Sign up</AuthSubmitButton>
-                <span className="signIn-option">
-                  Already a member?<Link to="/login">Sign in </Link>
-                </span>
-              </div>
-            </form>
-            <p>
-              By clicking “Sign up” I agree to the readMore Terms of Service and
-              confirm that I am at least 13 years of age. Read our Privacy
-              Policy.
-            </p>
-          </div>
+    <Container fixed maxWidth="sm" classes={{ root: classes.container }}>
+      <h1>Sign up for readMore</h1>
+      <p className="subheading">
+        Sign up to start your reading challenge this year!
+      </p>
+      <div className="errors-container">
+        <ul className="errors" id="sign-up-errors"></ul>
+      </div>
+
+      <form className="signup-form" onSubmit={handleSubmit}>
+        <label htmlFor="user_name">Name</label>
+        <AuthTextField
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Name"
+          variant="outlined"
+        />
+        <label htmlFor="user_email">Email address</label>
+        <AuthTextField
+          placeholder="you@yours.com"
+          variant="outlined"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="user_password">Password</label>
+        <AuthTextField
+          type="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <div className="signup-btn-container">
+          <AuthSubmitButton>Sign up</AuthSubmitButton>
+          <span className="signIn-option">
+            Already a member? <Link to="/login"> Sign in </Link>
+          </span>
         </div>
-      
+      </form>
+      <p id="privacy-notice">
+        By clicking “Sign up” I agree to the readMore Terms of Service and
+        confirm that I am at least 13 years of age. Read our Privacy Policy.
+      </p>
     </Container>
   );
 }
