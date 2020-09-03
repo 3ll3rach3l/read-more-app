ce7a2dc07507cb79a519c255c7b5714e02f0db57
Lauren <elle.rachel.writes@gmail.com>
2020-09-03 06:10:12 -1000

    Logout Button works and redirects

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import LogoutButton from './LogoutButton';
+import { Redirect } from 'react-router-dom'
+import {useSelector} from 'react-redux';
 
 function HomePage(){
+    const isLoggedIn = useSelector (state => !!state.auth.id);
+    
+
+    if(!isLoggedIn) return <Redirect to='/login'></Redirect>
 
    return(
        <>
