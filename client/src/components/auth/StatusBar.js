09f21cf4dfce1d6151aa9ae1787f6ba086393395
Lauren <elle.rachel.writes@gmail.com>
2020-09-04 14:21:04 -1000

    added StatusBar.js but will be changing it to 'leftContainer' since i havent actually added any functional components yet

---
+++ client/src/components/auth/StatusBar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+
+const StatusBar = () =>{
+
+return(
+    <div className="leftContainer">
+        <div className="readingChallengeDashboard">
+            <div className="challengeBanner">
+                <div className="challengeBannerContent">
+                    <div className="challengeProgressUser">
+                        <p>user avi goes here</p>
+                    </div>
+                    <div className="challengeProgress">
+                        <div className="challengeProgressBarProfile">
+                           <div className="challengeProgressBarHeader">
+                               
+                            </div> 
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+)
+
+}
+
+
+export default StatusBar;
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -5,6 +5,7 @@ import {useSelector} from 'react-redux';
 import NavBar from '../components/sitewide/NavBar'
 import Countdown from '../components/Countdown'
 import './cssPages/HomePage.css'
+import StatusBar from '../components/auth/StatusBar';
 
 function HomePage(){
     const isLoggedIn = useSelector (state => !!state.auth.id);
@@ -16,25 +17,32 @@ function HomePage(){
        <>
        <NavBar/>
        <main className="homepage-main">
-           <div className="banner">
-            <div className="banner-year">
-                       <img className="book-img" 
-                       src="https://s.gr-assets.com/assets/challenges/yearly/img_RCBook-626ef5100b911207af0c4d84f02df03a.svg" 
-                       alt="empty"/>
-                <div className="year">
-                    <span>
-                    2020 READING
-                    <br/>
-                    CHALLENGE
-                    </span>
+           <div className="mainContent">
+               <div className="mainContent-float">
+                    <div className="banner">
+                        <div className="banner-year">
+                             <img className="book-img" 
+                             src="https://s.gr-assets.com/assets/challenges/yearly/img_RCBook-626ef5100b911207af0c4d84f02df03a.svg" 
+                             alt="empty"/>
+                             <div className="year">
+                                 <span>
+                                     2020 READING
+                                     <br/>
+                                     CHALLENGE
+                                 </span>
+                             </div>
+                             <div className="countdown-container">
+                                 <h2>Time Remaining:</h2>
+                                 <Countdown />
+                             </div>
+                         </div>
+                     </div>
+                     <div className="challenge2020">
+                         <StatusBar/>
+                     </div>
+                     <div className="rightContainer"></div>
                 </div>
-            <div className="countdown-container">
-                <h2>Time Remaining:</h2>
-                <Countdown />
             </div>
-            </div>
-           </div>
-
        </main>
       </>
    )
