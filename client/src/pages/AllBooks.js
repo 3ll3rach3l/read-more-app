de134d5225db4e5da201936fbdc190d621463b74
Lauren <elle.rachel.writes@gmail.com>
2020-09-07 13:44:20 -1000

    had to fix redirect on books page..not that it matters

--- client/src/pages/AllBooks.js
+++ client/src/pages/AllBooks.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom'
+import { useSelector } from 'react-redux';
 import NavBar from '../components/sitewide/NavBar';
 
 
 const AllBooks = () => {
+    const isLoggedIn = useSelector(state => !!state.auth.id);
 
+    if (!isLoggedIn) return <Redirect to='/login'></Redirect>
     return(
         <>
             <NavBar></NavBar>
